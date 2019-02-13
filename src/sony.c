11384b39240f8ff1ea12c1498de94190b6046d5b
gnd <gnd@itchybit.org>
2019-02-13 21:31:29 +0100

    fixing sony - sony.c cleanup

--- src/sony.c
+++ src/sony.c
@@ -7,72 +7,17 @@
 #include <jerror.h>
 #include "sony.h"
 
-// TODO move this to the headerfile 
+// TODO move this to the headerfile
 static size_t SonyCallback(void *contents, size_t size, size_t nmemb, void* userp);
 
-static size_t SonyCallback(void *contents, size_t size, size_t nmemb, void* userp) {
-    size_t realsize = size * nmemb;
-    JpegMemory_t *mem = (JpegMemory_t *)userp;
+void *getJpegData(void *memory) {
+    JpegMemory_t *mem = (JpegMemory_t *)memory;
 
-    if (!mem->image_read) {
-      mem->memory = realloc(mem->memory, mem->size + realsize + 1);
-      if(mem->memory == NULL) {
-          printf("Not enough memory (realloc returned NULL)\n");
-          return 0;
-      }
-
-      memcpy(&(mem->memory[mem->size]), contents, realsize);
-      mem->size += realsize;
-      mem->memory[mem->size] = 0;
-    }
-
-    // did we read the payload header ?
-    if ((mem->size == 136) && !(mem->image_read)) {
-        mem->size_string = malloc(16);
-        sprintf(mem->size_string,"%02hhx%02hhx%02hhx", (unsigned char) mem->memory[12], (unsigned char) mem->memory[13], (unsigned char) mem->memory[14]);
-        mem->jpeg_size = (int)strtol(mem->size_string, NULL, 16);
-        mem->header_found = true;
-    }
-
-    // read the jpeg data
-    if ((mem->size == 136 + mem->jpeg_size) && mem->header_found) {
-        mem->image_swapping = true;
-        *mem->ro_jpeg_size = mem->jpeg_size;
-        mem->ro_memory = malloc(*mem->ro_jpeg_size);
-        memcpy(&mem->ro_memory, mem->memory, *mem->ro_jpeg_size);
-        mem->image_swapping = false;
-        mem->image_read = true;
-        mem->header_found = false;
-        mem->size = 0;
-    }
-    return realsize;
-}
-
-
-void *getJpegDataThread(void *handle) {
-
-    CURL *curl_handle = (CURL *)handle;
-    curl_easy_perform(curl_handle);
-    //curl_easy_cleanup(curl_handle);
-    printf("am i ever here ?\n");
-    //curl_global_cleanup();
-
-    // make gcc happy
-    return 0;
-}
-
-
-
-void *getJpegDataThreadNext(void *memory) {
-    printf("Thread[]: Setting up callback\n");
-    JpegMemory_t *chunk = (JpegMemory_t *)memory;
-
-    curl_easy_setopt(chunk->handle, CURLOPT_NOSIGNAL, 1);
-    curl_easy_setopt(chunk->handle, CURLOPT_WRITEFUNCTION, SonyCallback);
-    curl_easy_setopt(chunk->handle, CURLOPT_WRITEDATA, chunk);
-    curl_easy_perform(chunk->handle);
-    printf("lalal\n");
-    curl_easy_cleanup(chunk->handle);
+    curl_easy_setopt(mem->curl_handle, CURLOPT_NOSIGNAL, 1);
+    curl_easy_setopt(mem->curl_handle, CURLOPT_WRITEFUNCTION, SonyCallback);
+    curl_easy_setopt(mem->curl_handle, CURLOPT_WRITEDATA, mem);
+    curl_easy_perform(mem->curl_handle);
+    curl_easy_cleanup(mem->curl_handle);
     curl_global_cleanup();
 
     // make gcc happy
@@ -80,39 +25,58 @@ void *getJpegDataThreadNext(void *memory) {
 }
 
 
-/* this loads the JPEG data into the char* data array */
+static size_t SonyCallback(void *contents, size_t size, size_t nmemb, void* userp) {
+    size_t realsize = size * nmemb;
+    JpegMemory_t *mem = (JpegMemory_t *)userp;
+
+    mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+    if(mem->memory == NULL) {
+        printf("Not enough memory (realloc returned NULL)\n");
+        return 0;
+    }
+
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    // did we read the payload header ?
+    if (mem->size == 136) {
+        sprintf(mem->size_string,"%02hhx%02hhx%02hhx", (unsigned char) mem->memory[12], (unsigned char) mem->memory[13], (unsigned char) mem->memory[14]);
+        mem->jpeg_size = (int)strtol(mem->size_string, NULL, 16);
+        mem->header_found = true;
+    }
+
+    // read the jpeg data
+    if ((mem->size == 136 + mem->jpeg_size) && mem->header_found) {
+        // Lock mem and read data
+        pthread_mutex_lock(&video_mutex);
+        LoadJPEG(&mem->memory[136], &jpeg_dec, mem->jpeg_size);
+        pthread_mutex_unlock(&video_mutex);
+
+        // start loading another image
+        mem->size = 0;
+        return realsize;
+    }
+    return realsize;
+}
+
+
 void LoadJPEG(const unsigned char * imgdata, JpegDec_t* jpeg_dec, size_t jpeg_size) {
+  struct jpeg_decompress_struct info;
+  struct jpeg_error_mgr err;
 
-  printf("Entered LoadJPEG, jpeg size is %lu\n", jpeg_size);
-
-  struct jpeg_decompress_struct info;  //the jpeg decompress info
-  struct jpeg_error_mgr err;           //the error handler
-
-  info.err = jpeg_std_error(&err);     //tell the jpeg decompression handler to send the errors to err
-  jpeg_create_decompress(&info);       //sets info to all the default stuff
+  info.err = jpeg_std_error(&err);
+  jpeg_create_decompress(&info);
   jpeg_mem_src(&info, imgdata, jpeg_size);
-  jpeg_read_header(&info, TRUE);   //tell it to start reading it
-
-  printf("setting up some variables\n");
-  // speed things up ?
+  jpeg_read_header(&info, TRUE);
   info.do_fancy_upsampling = FALSE;
+  // Free previous jpeg_dec memory (if any)
+  free(jpeg_dec->data)
 
-  printf("going to decompress now\n");
-  jpeg_start_decompress(&info);    //decompress the file
-
-  printf("decompressed ! \n");
+  jpeg_start_decompress(&info);
   jpeg_dec->x = info.output_width;
   jpeg_dec->y = info.output_height;
   jpeg_dec->channels = info.num_components;
-
-  /* this makes sense only with opengl
-  //type = GL_RGB;
-
-  if(channels == 4) {
-    type = GL_RGBA;
-  }
-  */
-
   jpeg_dec->bpp = jpeg_dec->channels * 8;
   jpeg_dec->size = jpeg_dec->x * jpeg_dec->y * 3;
   jpeg_dec->data = malloc(jpeg_dec->size);
